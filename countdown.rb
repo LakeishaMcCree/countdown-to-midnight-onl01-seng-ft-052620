c893894d4c64bb3b3028017eefdae8b9d27582f5
Lakeisha McCree <lmccree215@icloud.com>
2020-05-28 04:07:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,7 +10,8 @@ end
 
 def countdown_with_sleep(seconds)
   while seconds > 0 
-  puts "#{seconds} SECOND(S)!"
-  seconds -= 1 
-  
-  
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1 
+    sleep (1)
+  end
+end 
