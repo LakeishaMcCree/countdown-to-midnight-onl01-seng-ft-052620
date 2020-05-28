4e5a31b8cc012b2a1cd12d04b79c506b40fc9817
Lakeisha McCree <lmccree215@icloud.com>
2020-05-28 03:59:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,12 @@
 #write your code here
 
 def countdown
-number = 10 
+  number = 10 
   while number > 0 
-  puts "#{number} SECOND(S)!"
-  number -= 1 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
   end
-  
+
+HAPPY NEW YEAR!
+
 end
